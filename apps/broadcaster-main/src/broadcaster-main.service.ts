a1349377f3d032e3098e06c53365521dcf0aeff3
Amrit Acharya <acharyaamrit47@gmail.com>
2025-11-29 11:08:52 +0545

    successfully receiving the modified data from eventreceiver

---
+++ apps/broadcaster-main/src/broadcaster-main.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from "@nestjs/common";
+import { ModifiedData } from "lib/common";
+
+@Injectable()
+export class BroadcasterMainService {
+
+
+    broadcastMarkedData(data: ModifiedData){
+        
+    }
+}
